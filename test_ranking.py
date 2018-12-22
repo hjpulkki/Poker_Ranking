9dde0fbf42f8df524b17996a1e7f9454fb6f9edf
Heikki Pulkkinen <h@eikki.net>
2018-12-22 23:09:21 +0200

    Simplified kicker score and fixed bug with kickers.

--- test_ranking.py
+++ test_ranking.py
@@ -18,6 +18,10 @@ def test_high_card_kicker():
     ace_high_kicker = th_poker_ranking.ranker('AC KC 2S 8H 4D')
     ace_high = th_poker_ranking.ranker('AC 3C 2S 8H 4D')
     assert ace_high_kicker > ace_high
+    
+    ace_high_kicker = th_poker_ranking.ranker('AC KC 2S 8H 4D')
+    ace_high_average_high = th_poker_ranking.ranker('AC QC JS TH 8D')
+    assert ace_high_kicker > ace_high_average_high
 
     
 def test_pairs_kicker():
@@ -25,6 +29,14 @@ def test_pairs_kicker():
     pair = th_poker_ranking.ranker('2S 2H 4D 5C KC')
     assert pair_kicker > pair
     
+    
+def test_wheel():
+    two_sight_straight = th_poker_ranking.ranker('2S 3H 4D 5C 6C')
+    wheel = th_poker_ranking.ranker('AS 2H 3D 4C 5C')
+    assert two_sight_straight > wheel
+    
+    three_of_kind = th_poker_ranking.ranker('2S 2H 2D 5C AC')
+    assert wheel > three_of_kind
     
 def test_hand_types():    
     straight_flush = th_poker_ranking.ranker('2C 3C 4C 5C 6C')
--- th_poker_ranking.py
+++ th_poker_ranking.py
@@ -28,112 +28,103 @@ card_values = dict((r, i) for i, r in enumerate('..23456789TJQKA'))
 max_value = max(card_values.values())
 
 
-def get_card_score(ordered_cards):
+def get_kicker_score(counter, hand_numbers):
     # Gives a minor score which is used to rank hand with kickers etc. correctly.
     # Inputs are the number values of card sorted from least important card to the hand
     # to the most important. Usually largest card is the most important, but pairs for
-    # example are an exception.
+    # example are an exception. Max value 759374 for 5 aces.
+    
+    counts_by_importance = sorted([(v, counter[v]*max_value + v) for v in hand_numbers], key=lambda x: x[1])
+    ordered_cards = [i[0] for i in counts_by_importance[:len(hand_numbers)]]
     
     score = 0
     for i, card_value in enumerate(ordered_cards):
-        weight = (max_value+1)**1  # ensures that a_1*weight_1 < a_2*weight_2 for any values of a_i
+        weight = (max_value+1)**i  # ensures that a_1*weight_1 < a_2*weight_2 for any values of a_i
         score += card_value*weight
     return score
+   
 
-
-def is_pairs(hand):
-    hand_numbers = [card_values[r] for r, s in hand]
-
-    counter = [0]*(max_value+1)
-    for v in hand_numbers:
-        counter[v] += 1
-        
-    # Sort primary by card count and secondary by card size
-    counts_by_importance = sorted([(v, counter[v]*max_value + v) for v in hand_numbers], key=lambda x: x[1])
-    score = get_card_score([i[0] for i in counts_by_importance[:len(hand)]])
-
+def is_pairs(counter):
     # 4 of a kind: 4 cards of the same number
     if 4 in counter:
-        return score + 70000
+        return 7000000
 
     # Full House: 3 of a Kind and 2 of a Kind
     if 3 in counter and 2 in counter:
-        return score + 60000
+        return 6000000
 
     # 3 of a Kind: 3 cards of the same number
     if 3 in counter:
-        return score + 30000
+        return 3000000
 
     # 2 Pairs: 2 Pairs of 2 of a kind (One Pair): 2 cards of the same number
     if counter.count(2) == 2:
-        return score + 20000
+        return 2000000
 
     # 2 of a Kind (One Pair): 2 cards of the same number
     if 2 in counter:
-        return score + 10000
+        return 1000000
 
-    return score
-
-
-def is_high_card(current_hand):
-    ordered_cards = sorted([card_values[r] for r, s in current_hand])
-    score = get_card_score(ordered_cards)
-    return score
+    return 0
 
 
-def is_flush(hand):
+def is_flush(hand, length=5):
     # find all suits in hand and count to see if there are 5 or greater
     # find all suit values in hand and return a list of all cards containing that suit (5, 6, or 7 possible cards)
     suits = [s for r, s in hand]
 
-    for i in set(suits):
-        if suits.count(i) >= 5:
-            score = is_high_card(hand)
-            return score + 50000
+    for i in suits:
+        if suits.count(i) >= length:
+            return 5000000
         
     return 0
 
 
-def is_straight(hand):
+def is_straight(hand, length=5):
     hand_numbers = [card_values[r] for r, s in hand]
-    num_list = sorted(set(hand_numbers))
-    straight_hand_cards =[]
+    sorted_hand = sorted(set(hand_numbers))
+    
+    # Wheel: Ace to 5 straight. Highest card is 5
+    flag = True
+    for v in range(2,length+1):
+        if v not in sorted_hand:
+            flag = False
+            break
+    if flag and 14 in sorted_hand:
+        return 3500000
 
-    # can only be len(num_list) from 7 to 5 - straight has to be 5 cards in a row
-    # will account for multiple straights (2,3,4,5,6,7,8), then the whole list will be returned
-    # will return duplicate card values for the straight (to help match if straight/flush)
-    # counts Ace as 14 so does not count A, 2, 3, 4, 5 as a straight (using Ace Low Rule)
-    if len(num_list) >= 5:
-        for i in range(len(num_list) - 4):
-            temp = num_list[i:i+5]
-            if (temp[4] - temp[0]) == 4:
-                # have to account for multiple straights in a given set of 7, unable to return value at this point
-                straight_hand_cards += [j for j in hand if card_values[j[0]] in temp]
-
-    straight_hand_cards = sorted(set(straight_hand_cards))
-
-    if len(straight_hand_cards) >= 5:
-        score = max(hand_numbers)   # Max card determines how good the straight is
-        print(hand, score)
-        return score + 40000
+    for end_ind in range(len(sorted_hand),length-1,-1):
+        print(end_ind)
+        if sorted_hand[end_ind-1]-sorted_hand[end_ind-length] == length-1:
+            return 4000000
+        
     return 0
 
 
 def is_straight_flush(flush, straight):
     if ((flush > 0) and (straight > 0)):
-        return straight + 80000
+        return 8000000
     return 0
 
 
 def ranker(hand):
     hand = hand.split()
+  
+    hand_numbers = [card_values[r] for r, s in hand]
+    counter = [0]*(max_value+1)
+    for v in hand_numbers:
+        counter[v] += 1
+        
+    kicker_score = get_kicker_score(counter, hand_numbers)
     
-    high_card_suit = is_high_card(hand)
-    pairs = is_pairs(hand)
-    straight = is_straight(hand)
-    flush = is_flush(hand)
+    pairs = is_pairs(counter)
+    if pairs == 0:
+        straight = is_straight(hand)
+        flush = is_flush(hand)
+    else:
+        straight, flush = 0, 0
     straight_flush = is_straight_flush(flush, straight)
     
-    score = max(straight_flush, flush, straight, pairs, high_card_suit)
+    score = max(straight_flush, flush, straight, pairs) + kicker_score
 
     return score
